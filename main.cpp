983df76dc9a77e2f7085040f951900526fee37d9
Akın Kürşat Özkan <akinkursatozkan@gmail.com>
2023-02-17 14:28:21 +0300

    implement GLFW and GLEW initializations and open Test Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+static constexpr GLint WIDTH = 800, HEIGHT = 600;
+
+bool init_glfw()
+{
+    if(!glfwInit())
+    {
+        printf("GLFW initialization failed!");
+        glfwTerminate();
+        return false;
+    }
+
+    // Setup GLFW window properties
+    // OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core profile = No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow forward compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!mainWindow)
+    {
+        printf("GLFW Window creation failed!");
+        glfwTerminate();
+        return false;
+    }
+
+    // Get Buffer size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW initialization failed!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return false;
+    }
+
+    // Setup Viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    // Loop until window closed
+    while(!glfwWindowShouldClose(mainWindow))
+    {
+        // Get + Handle user input events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+    return true;
+}
+
+int init()
+{
+    return (init_glfw()) ? 0 : 1;
+}
+
+int main()
+{
+    auto result = init();
+
+    return result;
 }
